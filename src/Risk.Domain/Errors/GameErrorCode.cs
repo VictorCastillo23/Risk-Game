76d45b6825d68f8578157e3979cd41e3cd3c8cd0
victorJavier <manolocastillo391@gmail.com>
2026-08-31 20:07:44 -0600

    feat(domain): add ActorIsNeutral error code

--- src/Risk.Domain/Errors/GameErrorCode.cs
+++ src/Risk.Domain/Errors/GameErrorCode.cs
@@ -20,5 +20,6 @@ public enum GameErrorCode
     InvalidPlayerCount,
     InvalidTroopCount,
     NoPendingOccupation,
-    ReinforcementIncomplete
+    ReinforcementIncomplete,
+    ActorIsNeutral
 }
